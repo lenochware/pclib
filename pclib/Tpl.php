e1fbaf13130d9b3cf13ba43ecad3cbada8ea27e6
lenochware <lenochware@gmail.com>
2015-12-16 09:58:36 +0100

    fixed nl2br format html/xhtml

--- pclib/Tpl.php
+++ pclib/Tpl.php
@@ -1039,7 +1039,7 @@ protected function formatStr($s, $fmt)
 	$len = strlen($fmt);
 	for($i = 0; $i < $len; $i++) {
 		switch ($fmt{$i}) {
-		case "n": $s = nl2br($s); break;
+		case "n": $s = nl2br($s, $this->useXhtml); break;
 		case "h": $s = utf8_htmlspecialchars($s); break;
 		case "H": $s = strip_tags($s); break;
 		case "u": $s = utf8_strtoupper($s); break;
