e15c5d77635e06b075fe2cf82d3ceea403087736
lenochware <lenochware@gmail.com>
2017-01-06 10:45:26 +0100

    Added Validator and ValidatorBase

---
+++ pclib/Validator.php
@@ -0,0 +1,154 @@
+<?php 
+/**
+ * @file
+ * Validator for common validation rules.
+ * @author -dk-
+ * http://pclib.brambor.net/
+ */
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+
+namespace pclib;
+use pclib;
+
+/**
+ * Validator for common validation rules.
+ */
+class Validator extends system\ValidatorBase
+{
+	const PATTERN_EMAIL = '/^[_\w\.\-]+@[\w\.-]+\.[a-z]{2,6}$/';
+	const PATTERN_FILENAME = '/^[a-zA-Z0-9_.-]+$/';
+	const PATTERN_IDENTIFIER = '/^[a-z_][a-z0-9_]+$/i';
+	const PATTERN_TIME24 = '/^([01]?[0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?$/';
+
+	public $dateTimeFormat = '%d.%m.%Y %H:%M%:%S';
+
+	function __construct($path = '')
+	{
+		parent::__construct($path);
+
+		//naatavit primo do array?
+		$this->setRule('email', array($this, 'isEmail'), "Invalid email address!");
+		$this->setRule('url', array($this, 'isUrl'), "Invalid url!");
+		$this->setRule('date', array($this, 'isDateTime'), "Invalid date!");
+		$this->setRule('time', array($this, 'isTime'), "Invalid time!");
+		$this->setRule('file', array($this, 'matchShell'), "Bad file type!");
+		$this->setRule('pattern', array($this, 'matchPattern'), "Value does not match requested format!");
+		$this->setRule('range', array($this, 'inRange'), "Value is not in range '%2$s'!");
+		$this->setRule('number', array($this, 'isNumeric'), "Not a number!");
+		$this->setRule('integer', array($this, 'isNumericInt'), "Not an integer value!");
+	}
+
+	/** Rule handler: Match regexp pattern. */
+	function matchPattern($value, $pattern)
+	{
+		return (bool)preg_match('~^'.$pattern.'$~', $value);
+	}
+
+	/** Rule handler: Email address. */
+	function isEmail($value)
+	{
+		return (bool)filter_var($value, FILTER_VALIDATE_EMAIL);
+	}
+
+	/** Rule handler: URL starting http://. */
+	function isUrl($value)
+	{
+		return (bool)filter_var($value, FILTER_VALIDATE_URL);
+	}
+
+	/** Rule handler: Match php identifier. */
+	function isIdentifier($value)
+	{
+		return (bool)preg_match(self::PATTERN_IDENTIFIER, $value);
+	}
+
+	/** Rule handler: Match wildcards. */
+	function matchShell($value, $wildcards)
+	{
+		if ($wildcards == 1) $wildcards = '*';
+		$wildcards = explode(';', $wildcards);
+
+		foreach ($wildcards as $wildcard) {
+			if (fnmatch($wildcard, $value)) return true;
+		}
+
+		return false;
+	}
+
+	/** Rule handler: Match number or numeric string. */
+	function isNumeric($value)
+	{
+		return is_numeric($value);
+	}
+	
+	/** Rule handler: Match integer. */
+	function isNumericInt($value)
+	{
+		return (bool)filter_var($value, FILTER_VALIDATE_INT);
+	}
+
+	/** Rule handler: Check if value is in range [min, max]. */
+	function inRange($value, $range)
+	{
+		if (!is_array($range)) $range = $this->parseRange($range);
+		return ($range[0] <= $value and $range[1] >= $value);
+	}
+
+	protected function parseRange($s)
+	{
+		$a = explode('..', $s);
+		if ($a[0] == '') $a[0] = -INF;
+		if ($a[1] == '') $a[1] = INF;
+
+		return array((float)$a[0], (float)$a[1]);
+	}
+
+	/**
+ * Validate password. You can set minlength and characters required in password
+ * with extcharset attribute.
+ * @copydoc valid-rule
+ */
+// function password($value, $options)
+// {
+// 	if ($options == 1) $options = '8,0';
+// 	list($minlen,$xchars) = explode(',', $options);
+// 	if ($minlen and (utf8_strlen($value) < $minlen)) return false;
+// 	if ($xchars and ctype_alnum($value)) return false;
+// 	return true;
+// }
+
+protected function parseDate($datestr, $format)
+{
+	$fmtspec = array('d','m','Y','H','M','S');
+	$d = preg_split("/[^0-9]+/", $datestr, null, PREG_SPLIT_NO_EMPTY);
+	$f = array_flip(preg_split("/[^a-z]+/i", $format, null, PREG_SPLIT_NO_EMPTY));
+	$datearray = array();
+	foreach($fmtspec as $i) {
+		$datearray[] = isset($f[$i])? $d[$f[$i]] : strftime("%$i");
+	}
+	return $datearray;
+}
+
+/** Rule handler: Match datetime against format $format. */
+function isDateTime($value, $format = '')
+{
+	if (!$format or $format == 1) $format = $this->dateTimeFormat;
+	list($d,$m,$y,$h,$i,$s) = $this->parseDate($value, $format);
+	if (!checkdate($m,$d,$y)) return false;
+	if (isset($h) and !$this->isTime($h.':'.$i.':'.($s ?: '00'))) return false;
+	return true;
+}
+
+/** Rule handler: Match time in format HH:MM:SS. */
+function isTime($value)
+{
+	return (bool)preg_match(self::PATTERN_TIME24, $value);
+}
+
+}
+
+?>
---
+++ pclib/system/ValidatorBase.php
@@ -0,0 +1,239 @@
+<?php 
+/**
+ * @file
+ * Base class for any pclib Validator.
+ * @author -dk-
+ * http://pclib.brambor.net/
+ */
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+
+namespace pclib\system;
+
+/**
+ * Base class for any pclib Validator.
+ * Features:
+ * - Definition of validation rules in template
+ * - Method isValid() for validation just one value
+ * - Method validate() for validation array of values against template
+ * - Method getErrors() for reading validation errors
+ * - Method setRule() for adding your own rules
+ */
+class ValidatorBase extends BaseObject
+{
+ 
+ /** Are variables undefined in template valid? */
+ public $skipUndefined = true;
+
+ /** Silently skip unknown rules? */
+ public $skipUndefinedRule = true;
+
+/** List of ignored attributes (rules) */
+ public $ignoredAttributes = array('id', 'type', 'begin', 'end');
+
+/** List of ignored element types */
+ public $ignoredElements = array();
+
+/** Array of messages [ruleName: message, ...] */
+	public $messages = array(
+		'undefined' => "Undefined element '{name}'",
+	);
+
+	/** Array of rule handlers [ruleName: callable, ...] */
+	protected $rules = array();
+
+	/** Array of [fieldName: errorMessage, ...] */
+	protected $errors;
+
+	protected $elements;
+
+	/**
+	 * Create validator.
+	 * Get template object or path to template and load it.
+	 */
+	function __construct()
+	{
+		parent::__construct();
+		$this->setRule('required', array($this, 'notBlank'), "'%s' is required!");
+	}
+
+	/**
+	 * Set or add new rule.
+	 * @param string $name Rule name
+	 * @param callable $function Rule handler
+	 * @param string $message Rule error message
+	 */
+	function setRule($name, /*callable*/ $function, $message)
+	{
+		if (!is_callable($function)) {
+			throw new Exception("Rule handler must be callable.");
+		}
+
+		$this->rules[$name] = $function;
+		$this->messages[$name] = $message;
+	}
+
+	/**
+	 * Check if validator has handler for rule $rule.
+	 * @param string $rule Rule name
+	 */
+	function hasRule($rule)
+	{
+		return is_callable($this->rules[$rule]);
+	}
+
+	/**
+	 * Set error message for element $name, rule $rule and value $value.
+	 * Called when validation of element's value failed.
+	 */
+	function setError($name, $value, $rule)
+	{
+		$mEl = $this->elements[$name.'.'.$rule];
+
+		if ($mEl['type'] == 'message') {
+			$message = $mEl['text'];
+		}
+		else {
+			$message = $this->messages[$rule] ?: sprintf("%s: Validation of '%s' failed,", $name, $rule);
+		}
+
+		$this->errors[$name] = sprintf($message, $name, $value);
+	}
+
+	/** 
+	 * Return validation errors.
+	 */
+	function getErrors()
+	{
+		return $this->errors;
+	}
+
+	/** 
+	 * Check if field is blank (not filled). 
+	 */
+	function isBlank($value)
+	{
+		return is_array($value)? (count($value) == 0) : (strlen($value) == 0);
+	}
+
+	/** 
+	 * NOT isBlank().
+	 */
+	function notBlank($value)
+	{
+		return !$this->isBlank($value);
+	}
+
+	function filter($value, $rules)
+	{
+		return $value;
+	}
+
+	/**
+	 * Validate $value using $rule.
+	 * Example: validateRule('1.1.2016', 'date', '%d.%m.%Y')
+	 * See also isValid()
+	 * @param mixed $value
+	 * @param string $rule
+	 * @param mixed $param Rule parameter
+	 * @return bool isValid
+	 */
+	function validateRule($value, $rule, $param = null)
+	{
+		$func = $this->rules[$rule];
+
+		if (is_callable($func)) {
+			return call_user_func($func, $value, $param);
+		}
+		else {
+			throw new Exception(sprintf("Rule '%s' is not defined.", $rule));			
+		}
+	}
+
+	function validate($value, $rules)
+	{
+		$elem = array_flip(explode(' ', $rules));
+		$elem['type'] = 'string';
+
+		return $this->validateElement($value, $elem);
+
+	}
+
+	/**
+	 * Validate $value against $rules.
+	 * Example: validate('1.1.2016', 'date')
+	 * @param mixed $value
+	 * @param string|array $rules
+	 * @return bool isValid
+	 */
+	function validateElement($value, array $elem)
+	{
+		if (!$elem['type']) {
+			if ($this->skipUndefined) {
+				return true;
+			}
+			else {
+				$this->setError($elem['id'], $value, 'undefined');
+				return false;
+			}
+		}
+
+		//blank fields handling: required: invalid, not-required: valid.
+		if ($this->isBlank($value)) {
+			if ($elem['required']) {
+				$this->setError($elem['id'], $value, 'required');
+				return false;
+
+			}
+			return true;
+		}
+
+		foreach ((array)$elem as $rule => $param) {
+			if (!$this->hasRule($rule)) {
+				 if ($this->skipUndefinedRule or in_array($rule, $this->ignoredAttributes)) {
+					continue;
+				}
+			}
+
+			if (!$this->validateRule($value, $rule, $param)) {
+				$this->setError($name, $value, $rule);
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+/**
+	 * Validate array of values, using validation rules in template.
+	 * Set $this->errors array.
+	 * @param array $values
+	 * @param array $elements
+	 * @return bool isValid
+	 */
+	function validateArray(array $values, array $elements)
+	{
+		$ok = true;
+		$this->errors = array();
+		$this->elements = $elements;
+
+		$keys = array_unique(
+			array_merge(array_keys($values), 
+			array_keys($elements))
+		);
+
+		foreach ($keys as $key) {
+			if (in_array($elements[$key]['type'], $this->ignoredElements)) continue;
+			if (!$this->validateElement($values[$key], $elements[$key])) $ok = false;
+		}
+
+		return $ok;
+	}
+
+
+}
+
+?>
