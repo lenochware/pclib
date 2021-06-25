557b0cd7ffb1b5a438d200f02867d1868954ae64
lenochware <lenochware@gmail.com>
2021-06-25 18:04:46 +0200

    short array syntax

--- pclib/Config.php
+++ pclib/Config.php
@@ -13,23 +13,23 @@
 # version 2.1 of the License, or (at your option) any later version.
 
 /** pclib default configuration */
-$config = array(
-	'pclib.errors' => array('display', 'develop', /*,'log','template'=>'error.tpl' */),
-	'pclib.locale' => array('date' => '%d. %m. %Y', 'datetime' => '%d.%m.%Y %H:%M%:%S'),
-	'pclib.logger' => array('log' => ['ALL']),
+$config = [
+	'pclib.errors' => ['display', 'develop', /*,'log','template'=>'error.tpl' */],
+	'pclib.locale' => ['date' => '%d. %m. %Y', 'datetime' => '%d.%m.%Y %H:%M%:%S'],
+	'pclib.logger' => ['log' => ['ALL']],
 
-	'pclib.directories' => array(
+	'pclib.directories' => [
 		'logs' => 'temp/log/',
 		'assets' => '{pclib}/assets/',
 		'localization' => '{webroot}{pclib}/localization/',
-	),
+	],
 
-	'pclib.loader' => array(
-		'controller' => array('dir' => 'controllers', 'namespace' => '', 'postfix' => 'Controller'),
-	),
+	'pclib.loader' => [
+		'controller' => ['dir' => 'controllers', 'namespace' => '', 'postfix' => 'Controller'],
+	],
 
-	'pclib.security' => array('tpl-escape' => false, 'csrf' => false, 'form-prevent-mass' => false),
-	'pclib.auth' => array('algo' => 'md5', 'secret' => 'write any random string!', 'realm' => ''),
-);
+	'pclib.security' => ['tpl-escape' => false, 'csrf' => false, 'form-prevent-mass' => false],
+	'pclib.auth' => ['algo' => 'md5', 'secret' => 'write any random string!', 'realm' => ''],
+];
 
 ?>
